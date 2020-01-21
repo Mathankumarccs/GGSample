ab0adb86ffadc7546f7361e32f9dd0862abaeeea
Mathankumar C <mathankumarccs1993@gmail.com>
2020-01-21 15:05:34 +0530

    Initial Commit

---
+++ GGSample/Assets/Script/AdsRefresh.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GreedyGame.Runtime;
+
+public class AdsRefresh : MonoBehaviour
+{
+    public void buttonclick()
+	{
+		GreedyGameAgent.Instance.startEventRefresh();
+		Debug.Log("GG[Ads] Refresh");
+
+	}
+}
---
+++ GGSample/Assets/Script/PopUpScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using GreedyGame.Runtime;
+using UnityEngine;
+
+public class PopUpScript : MonoBehaviour
+{
+    public string unitId;
+
+    void OnMouseDown()
+    {
+
+        GreedyGameAgent.Instance.showEngagementWindow(unitId);
+
+    }
+
+   public void Buttonclick()
+    {
+        GreedyGameAgent.Instance.showEngagementWindow(unitId);
+    }
+}
